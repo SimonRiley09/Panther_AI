3d661c921ff15113a52ab468d9e0945cc573a67e
SimonRiley09 <s.shamsipour09@gmail.com>
2024-12-17 09:25:01 -0500

    modified:   app.py

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-import datetime
-
-import helper
+from helper import login_required
 
 # Configure application
 app = Flask(__name__)
